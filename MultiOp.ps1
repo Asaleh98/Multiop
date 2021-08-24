5827a59f38be7188bcab1eaf9cf053c5b0136d6a
Asaleh98 <89383713+Asaleh98@users.noreply.github.com>
2021-08-24 10:51:28 +0100

    Add files via upload

--- MultiOp.ps1
+++ MultiOp.ps1
@@ -5,11 +5,19 @@ $SECOND_NUM = $SECOND_NUM -as [int]
 $SUM = $FIRST_NUM + $SECOND_NUM
 $SUB = $FIRST_NUM - $SECOND_NUM
 $MUL = $FIRST_NUM * $SECOND_NUM
-$DIV = $FIRST_NUM / $SECOND_NUM
+if($SECOND_NUM -ne 0)
+{
+	$DIV = $FIRST_NUM / $SECOND_NUM
+	Write-Host $FIRST_NUM "/" $SECOND_NUM "=" $DIV
+}
+else
+{
+	Write-Host "Division by 0 is not possible"
+}
+
 Write-Host $FIRST_NUM "+" $SECOND_NUM "=" $SUM
 Write-Host $FIRST_NUM "-" $SECOND_NUM "=" $SUB
 Write-Host $FIRST_NUM "*" $SECOND_NUM "=" $MUL
-Write-Host $FIRST_NUM "/" $SECOND_NUM "=" $DIV
 Add-Content -Path "C:\Users\Abdulaleem Saleh\Desktop\Results.txt" -Value $SUM 
 Add-Content -Path "C:\Users\Abdulaleem Saleh\Desktop\Results.txt" -Value $SUB 
 Add-Content -Path "C:\Users\Abdulaleem Saleh\Desktop\Results.txt" -Value $MUL 
